7e5957cbb53393bca2720eaacb395c3ee786037c
NakanoMakoto <m.nakano8649@gmail.com>
2019-03-06 23:33:12 +0900

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `まぶろぐ`,
+    description: `This is Makoto Nakano's homepage.`,
     author: `@gatsbyjs`,
     link: [
       `top`,
