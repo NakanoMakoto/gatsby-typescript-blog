0164fdb50ca368265d182603c1c8f25907187124
nakanomakoto <m.nakano8649@gmail.com>
2019-05-03 14:23:05 +0900

    create post detail pages

--- src/templetes/post.tsx
+++ src/templetes/post.tsx
@@ -1,15 +1,65 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const PostPage = () => (
-  <Layout>
-    <SEO title="posts" />
-    <h1> This is blog post Page</h1>
-    <Link to="/"> Go Back</Link>
-  </Layout>
-);
+const PostPage = (query: postQuery) => {
+  const data: postData = query.data.allMarkdownRemark.edges[0].node;
+  return (
+    <Layout>
+      <SEO title="posts" />
+      <h1>{data.frontmatter.title}</h1>
+      <div>{data.frontmatter.date}</div>
+      <div>{data.excerpt}</div>
+      <Link to="/"> Go Back</Link>
+    </Layout>
+  );
+};
+
+interface postData {
+    "frontmatter": {
+      "title": string,
+      "date": string,
+      "slug": string
+    },
+    "excerpt": string
+}
+
+interface postQuery {
+  "data": {
+    "allMarkdownRemark": {
+      "edges": {
+        "node": postData[]
+      }
+    }
+  }
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          slug: {
+            eq: $slug
+          }
+        }
+      }
+    ){
+        totalCount
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+              slug
+            },
+            excerpt
+          }
+        }
+      }
+  }
+`;
 
 export default PostPage;
